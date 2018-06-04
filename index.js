a20d2659e3b86557f4b0e9a48e9c5d82f15664ca
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-04 23:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function add2(n) {
   return n + two;
 }
 
-var theFunk = funkyFunction()();
 var funkyFunction = function() {
   return function() {
     return "FUNKY!";
-  }
-}
+  };
+};
+var theFunk = funkyFunction()();
